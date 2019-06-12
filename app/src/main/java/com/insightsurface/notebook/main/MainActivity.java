465a094357af358c7d2451fbb40855b15b26b752
suhang <suhang@xianhuahua.com>
2019-06-12 18:16:44 +0800

    Initial commit

---
+++ app/src/main/java/com/insightsurface/notebook/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.insightsurface.notebook.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.insightsurface.notebook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
